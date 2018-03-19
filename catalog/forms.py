f3aea0ac31f4759ce1987c74a343015a619242e8
Brad <bhamer477@gmail.com>
2018-03-19 10:36:44 -0700

    File Changes All Over

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,3 @@
-
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 import datetime #for checking renewal date range.
--- locallibrary/wsgi.py
+++ locallibrary/wsgi.py
@@ -1,6 +1,8 @@
 """
 WSGI config for locallibrary project.
+
 It exposes the WSGI callable as a module-level variable named ``application``.
+
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
